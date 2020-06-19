9bfab3bf5f7c6cccb4baa8b67d03e39e2dacfdb8
yogesh <yogeshmathi12@gmail.com>
2020-06-19 15:38:28 +0530

    commiting after adding linear_search function in the time complexity program

--- time_complexity.py
+++ time_complexity.py
@@ -5,15 +5,18 @@ class Time_complexity:
         self.lst=lst
 
 
-    def constant_time(self): #constant_time=O(1)
+    def constant_time(self):         #constant_time=O(1)
         index_0=self.lst[0]
         print(index_0)
 
-    def linear_time(self):   #linear_time=O(n)
-        for num in self.lst:
-            print(num)
+    def linear_search(self, value):  #linear_time=O(n)
+        for index in range(len(self.lst)):
+            if value == self.lst[index]:
+                print(index)
+                return
+        print('Value not found in the list')
 
-    def bubble_sort(self):   #quadratic_time=O(n^2)
+    def bubble_sort(self):           #quadratic_time=O(n^2)
         swapped = True
         while swapped:
             swapped = False
@@ -23,7 +26,7 @@ class Time_complexity:
                     swapped = True
         print(self.lst)
 
-    def binary_search(self,value): #logarithmic_time=O(log n)
+    def binary_search(self,value):  #logarithmic_time=O(log n)
         n = len(self.lst)
         left = 0
         right = n - 1
@@ -38,15 +41,15 @@ class Time_complexity:
                 return
         print('Value is not in the list')
 
-    def quasilinear_time(self): #quasilinear_time=O(n log n)
+    def quasilinear_time(self):     #quasilinear_time=O(n log n)
         for value in self.lst:
             t1.binary_search(value)
 
 
-    def factorial_time(self,n): #factorial_time=O(n!)
+    def factorial_time(self,n):     #factorial_time=O(n!)
         pass
 
-    def my_function(self): #it has multiple time complexities: O(1) + O(n) + O(n^2)
+    def my_function(self):          #it has multiple time complexities: O(1) + O(n) + O(n^2)
         first_element =self.lst[0]  #O(1)
         print(first_element)
 
@@ -55,7 +58,7 @@ class Time_complexity:
 
         for x in self.lst:          #O(n^2)
             for y in self.lst:
-                print(x, y)    #we take worst case as the time complexity so here: O(n^2)
+                print(x, y)         #we take worst case as the time complexity so here: O(n^2)
 
 lst=[3,5,8,4,9,1]
 t1=Time_complexity(lst)
@@ -64,6 +67,6 @@ t1.bubble_sort()
 #t1.linear_time()
 #t1.binary_search(9)
 #t1.quasilinear_time()
-#t1.linear_time()
+#t1.linear_search(1)
 
 
