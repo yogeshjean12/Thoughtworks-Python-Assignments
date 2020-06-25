8ca5c60e6200ce011659d818e6e2b8f0d3cb9de1
yogesh <yogeshmathi12@gmail.com>
2020-06-25 17:38:02 +0530

    commiting anagram program

---
+++ anagram.py
@@ -0,0 +1,14 @@
+
+input1=input("Enter the first string: ")
+input2=input("Enter the second string: ")
+lst=list(input2)
+for i in input1:
+    if i!=' ':
+        if i in input2:
+            lst.remove(i)
+        else:
+            print("False")
+            break
+
+else:
+    print('True')
