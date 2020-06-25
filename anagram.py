0a6d916030711adf5c7c46d3fe9d2a18f066f45f
yogesh <yogeshmathi12@gmail.com>
2020-06-25 17:44:30 +0530

    modified anagram program

--- anagram.py
+++ anagram.py
@@ -1,10 +1,11 @@
 
 input1=input("Enter the first string: ")
 input2=input("Enter the second string: ")
+
 lst=list(input2)
 for i in input1:
     if i!=' ':
-        if i in input2:
+        if i in lst:
             lst.remove(i)
         else:
             print("False")
