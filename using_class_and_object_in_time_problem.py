2c9ff40c6506a93b1d1b83da5a27c2d90e1c696a
Yogesh Jean <yogeshmathi12@gmail.com>
2020-07-15 21:29:08 +0530

    updating oops section-1 assignment problem

--- using_class_and_object_in_time_problem.py
+++ using_class_and_object_in_time_problem.py
@@ -1,52 +1,50 @@
 import sys
 
 class Time:
-    ''' Time related operations '''
-    hours = 24
-    minutes = 60
+    ''' Time '''
+    valid_hours = 24
+    valid_minutes = 60
 
-    def __init__(self, first_time, second_time):
-        if (first_time[0] <= self.hours and first_time[0] >= 0 ) and (first_time[1] <= self.minutes and first_time[1] >= 0):
-            self.first_time = first_time
+    def __init__(self, time):
+        if (time[0] <= self.valid_hours and time[0] >= 0 ) and (time[1] <= self.valid_minutes and time[1] >= 0):
+            self.hour = time[0]
+            self.mins = time[1]
+            self.time_in_minutes =self.hour*self.valid_minutes + self.mins
         else:
-            sys.exit('Error in the first time format, Try again with the correct time format')
-        if (second_time[0] <= self.hours and second_time[0] >= 0) and (second_time[1] <= self.minutes and second_time[1] >= 0):
-            self.second_time = second_time
-        else:
-            sys.exit('Error in the second time format, Try again with the correct time format')
+            sys.exit('Error in the time format, Try again with the correct time format')
 
-    def add_time(self):
-        hour = self.first_time[0] + self.second_time[0]
-        mins = self.first_time[1] + self.second_time[1]
+def add_time(t1,t2):
+        hour = t1.hour + t2.hour
+        mins = t1.mins + t2.mins
         if mins >= 60:
             mins -= 60
             hour += 1
         return 'Concatenated time is {}hr and {}mins'.format(hour,mins)
 
-    def time_difference(self):
-        if self.first_time[0] >= self.second_time[0]:
-            hour = self.first_time[0] - self.second_time[0]
-        else:
-            hour = self.second_time[0] - self.first_time[0]
+def total_time_in_minutes(t1,t2):
+    return 'Total time taken is {}minutes'.format(t1.time_in_minutes + t2.time_in_minutes)
 
-        if self.first_time[1] >= self.second_time[1]:
-            mins = self.first_time[1] - self.second_time[1]
+def time_difference(t1, t2):
+        if t1.hour >= t2.hour:
+            hour = t1.hour - t2.hour
         else:
-            mins = self.second_time[1] - self.first_time[1]
+            hour = t2.hour - t1.hour
+
+        if t1.mins >= t2.mins:
+            mins = t1.mins - t2.mins
+        else:
+            mins = t2.mins - t1.mins
 
         return 'Time difference is {}hr and {}mins'.format(hour,mins)
 
-    def time_in_minutes(self):
-        mins = (self.first_time[0]*self.minutes)+(self.second_time[0]*self.minutes)+(self.first_time[1]+self.second_time[1])
-        return 'Total time taken in minutes is {}mins'.format(mins)
 try:
     print('Use 24 hours time format')
-    first_time=list(map(int,input('Enter the first time separated by colon(:) Eg.5:30 : ').strip().split(':')))
-    second_time=list(map(int,input('Enter the second time separated by colon(:) Eg.2:15 : ').strip().split(':')))
+    t1 = Time(list(map(int,input('Enter the first time separated by colon(:) Eg.5:30 : ').strip().split(':'))))
+    t2 = Time(list(map(int,input('Enter the first time separated by colon(:) Eg.5:30 : ').strip().split(':'))))
+
 except Exception:
     print('Wrong input format, Enter the input as specified, Try again with the correct input format')
 else:
-    Time_1 = Time(first_time, second_time)
-    print(Time_1.add_time())
-    print(Time_1.time_difference())
-    print(Time_1.time_in_minutes())
+    print(add_time(t1, t2))
+    print(total_time_in_minutes(t1, t2))
+    print(time_difference(t1, t2))
